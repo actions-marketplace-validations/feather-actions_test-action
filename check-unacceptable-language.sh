e950c6393cbc66dd6451a6aa190500e203d327c0
GErP83 <gurrka@gmail.com>
2024-05-30 17:58:24 +0200

    Update check-unacceptable-language.sh

--- check-unacceptable-language.sh
+++ check-unacceptable-language.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-log() { printf "** %s\n" "$*" >&2; }
-error() { printf "** ERROR: %s\n" "$*" >&2; }
+log() { printf -- "** %s\n" "$*" >&2; }
+error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
 REPO_ROOT="$(git -C "$PWD" rev-parse --show-toplevel)"
@@ -10,16 +10,26 @@ UNACCEPTABLE_LANGUAGE_PATTERNS_PATH="${REPO_ROOT}/unacceptable-language.txt"
 
 log "Checking for unacceptable language..."
 
-if ! test -f "${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}"; then
+if ! test -f ${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH};
+then
   CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   UNACCEPTABLE_LANGUAGE_PATTERNS_PATH="${CURRENT_SCRIPT_DIR}/unacceptable-language.txt"
-  log "⚠️ 'unacceptable-language.txt' not found in repository, using the one included with the GitHub action."
-fi
+  log "⚠️ There was no 'unacceptable-language.txt' file in the repository, so we use the one that is included with the github action."
 
-PATHS_WITH_UNACCEPTABLE_LANGUAGE=$(git -C "${REPO_ROOT}" grep -l -F -w -f "${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}" -- ":(exclude)${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}" || true | paste -s -d " ")
+  PATHS_WITH_UNACCEPTABLE_LANGUAGE=$(git -C "${REPO_ROOT}" grep \
+  -l -F -w \
+  -f "${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}" \
+   ) || true | /usr/bin/paste -s -d " " -
+else
+  PATHS_WITH_UNACCEPTABLE_LANGUAGE=$(git -C "${REPO_ROOT}" grep \
+  -l -F -w \
+  -f "${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}" \
+  -- ":(exclude)${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}" \
+  ) || true | /usr/bin/paste -s -d " " -
+fi
 
 if [ -n "${PATHS_WITH_UNACCEPTABLE_LANGUAGE}" ]; then
   fatal "❌ Found unacceptable language in files:\n${PATHS_WITH_UNACCEPTABLE_LANGUAGE}."
-else
-  log "✅ Found no unacceptable language."
 fi
+
+log "✅ Found no unacceptable language."
