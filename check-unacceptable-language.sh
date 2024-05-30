584cd80f80b0044eeb7250092547dc63f8ddcf5f
GErP83 <gurrka@gmail.com>
2024-05-30 18:13:40 +0200

    Update check-unacceptable-language.sh

--- check-unacceptable-language.sh
+++ check-unacceptable-language.sh
@@ -7,25 +7,31 @@ fatal() { error "$@"; exit 1; }
 
 REPO_ROOT="$(git -C "$PWD" rev-parse --show-toplevel)"
 UNACCEPTABLE_LANGUAGE_PATTERNS_PATH="${REPO_ROOT}/unacceptable-language.txt"
-EXCLUDE_COMMAND=(-- ":(exclude)${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}")
-
-if ! test -f ${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}; then
-  EXCLUDE_COMMAND = (:)
-  CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  UNACCEPTABLE_LANGUAGE_PATTERNS_PATH="${CURRENT_SCRIPT_DIR}/unacceptable-language.txt"
-  log "⚠️ There was no 'unacceptable-language.txt' file in the repository, so we use the one that is included with the github action."
-fi
 
 log "Checking for unacceptable language..."
 
-PATHS_WITH_UNACCEPTABLE_LANGUAGE=$(git -C "${REPO_ROOT}" grep \
--l -F -w \
--f "${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}" \
-"${EXCLUDE_COMMAND[@]}" \
-) || true | /usr/bin/paste -s -d " " -
+if ! test -f ${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}; then
+  CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  UNACCEPTABLE_LANGUAGE_PATTERNS_PATH="${CURRENT_SCRIPT_DIR}/unacceptable-language.txt"
+  log "❗ There was no 'unacceptable-language.txt' file in the repository, so we use the one that is included with the github action."
+
+  PATHS_WITH_UNACCEPTABLE_LANGUAGE=$(git -C "${REPO_ROOT}" grep \
+  -l -F -w \
+  -f "${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}" \
+  ) || true | /usr/bin/paste -s -d " " -
+
+else
+  PATHS_WITH_UNACCEPTABLE_LANGUAGE=$(git -C "${REPO_ROOT}" grep \
+  -l -F -w \
+  -f "${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}" \
+  -- ":(exclude)${UNACCEPTABLE_LANGUAGE_PATTERNS_PATH}" \
+  ) || true | /usr/bin/paste -s -d " " -
+
+fi
 
 if [ -n "${PATHS_WITH_UNACCEPTABLE_LANGUAGE}" ]; then
-  fatal "❌ Found unacceptable language in files: ${PATHS_WITH_UNACCEPTABLE_LANGUAGE}."
+  log "❌ Found unacceptable language in files:"
+  fatal "${PATHS_WITH_UNACCEPTABLE_LANGUAGE}"
 fi
 
 log "✅ Found no unacceptable language."
